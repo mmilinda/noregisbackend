cac5fcffa245f96aa562e97b6467eb94de382547
Gervais6 <102412053+gervais6@users.noreply.github.com>
2026-05-09 17:46:59 +0000

    ajout axio

--- models/Visiteur.js
+++ models/Visiteur.js
@@ -1,4 +1,3 @@
-// models/Visiteur.js
 const mongoose = require('mongoose');
 
 const visiteurSchema = new mongoose.Schema({
@@ -6,27 +5,7 @@ const visiteurSchema = new mongoose.Schema({
   prenom:        { type: String, required: true, maxlength: 100 },
   dateNaissance: { type: Date, default: null },
   numeroPiece:   { type: String, required: true, unique: true },
-  typePiece:     { 
-    type: String, 
-    enum: ['CNI', 'PASSEPORT', 'PERMIS', 'CARTE_SEJOUR'], 
-    default: 'CNI' 
-  },
+  typePiece:     { type: String, enum: ['CNI', 'PASSEPORT', 'PERMIS', 'CARTE_SEJOUR'], default: 'CNI' },
 }, { timestamps: true });
-
-// Middleware pre-save : normalise le typePiece avant validation
-visiteurSchema.pre('save', function(next) {
-  const mapping = {
-    'Carte Nationale d\'Identité': 'CNI',
-    "Carte Nationale d'Identité": 'CNI',
-    'Carte Nationale d’Identité': 'CNI',
-    'Passeport': 'PASSEPORT',
-    'Permis de conduire': 'PERMIS',
-    'Carte de séjour': 'CARTE_SEJOUR',
-  };
-  if (mapping[this.typePiece]) {
-    this.typePiece = mapping[this.typePiece];
-  }
-  next();
-});
 
 module.exports = mongoose.model('Visiteur', visiteurSchema);
