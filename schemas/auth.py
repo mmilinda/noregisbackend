da99c4f0f3cb6de55b19e7f22f9602e775af04b9
Gervais6 <102412053+gervais6@users.noreply.github.com>
2026-05-18 16:59:20 +0000

    modif middlewares

--- schemas/auth.py
+++ schemas/auth.py
@@ -1,10 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional, Literal
 
 
 class LoginBody(BaseModel):
     email: str
-    password: str
+    # Accepte aussi bien "password" que "mot_de_passe"
+    password: str = Field(..., alias="mot_de_passe")
+
+    class Config:
+        populate_by_name = True   # permet d'utiliser "password" ou "mot_de_passe" indifféremment
 
 
 class RegisterBody(BaseModel):
